1cf73a162a6f4b966e19ed9cd02b17bb30f865cd
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-06-13 08:54:40 +0200

    fix typo in build-script

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ function get_required_kitsune_lib_repo () {
 
 #-----------------------------------------------------------------------------------------------------------------
 
-get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.18p.0" 4 "staticlib"
+get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.18.0" 4 "staticlib"
 
 get_required_kitsune_lib_repo "libKitsunemimiPersistence" "v0.10.1" 4 "staticlib"
 
